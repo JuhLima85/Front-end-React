490669ff958fb12cfb1bf682f61e2b6519d36d4d
JuhLima85 <limajuh17@gmail.com>
2023-06-26 10:42:41 -0300

    projeto

---
+++ src/components/NotificationButton/index.tsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
+import './styles.css';
+
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id : number){ 
+    axios(`${BASE_URL}/vendas/${id}/notificacao`)
+    .then(response => {
+        toast.info("Mensagem enviada com sucesso!");
+    });
+
+}
+
+function NotificationButton({saleId} : Props) {
+    return (
+        <div className= "listavendas-red-btn" onClick={() => handleClick(saleId)}>
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton;
+
